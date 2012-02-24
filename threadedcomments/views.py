8382dd65edc3ff8478362f762e232da9630b0325
Jordan Reiter <jordanreiter@gmail.com>
2012-02-24 14:33:12 -0500

    Update threadedcomments/views.py

--- threadedcomments/views.py
+++ threadedcomments/views.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from models import ThreadedComment
-from form import 
 
 def delete_comment(request, comment=None, template_name="comments/delete.html", next=None):
     comment = ThreadedComment.objects.get(pk=comment)
