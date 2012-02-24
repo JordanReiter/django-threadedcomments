1a5346f42064f18d9f664d16109c281adc26f677
MakeToLearn <jordan@aace.org>
2012-02-24 14:26:11 -0500

    Needs settings

--- threadedcomments/urls.py
+++ threadedcomments/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('threadedcomments.views',
-    url(r'^(?P<comment>\d+)/edit/$' 'edit_comment', name='comments-edit-comment'),
-    url(r'^(?P<comment>\d+)/delete/$', 'delete_comment', name='comments-delete-comment'),
+#    url(r'^(?P<comment>\d+)/edit/$' 'edit_comment', name='comments-edit-comment'),
+#    url(r'^(?P<comment>\d+)/delete/$', 'delete_comment', name='comments-delete-comment'),
 )
